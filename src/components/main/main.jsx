36134f516d6064a4b123fdcb31ddf6099571d736
germiona <violetta.ashirova@gmail.com>
2020-02-05 23:06:27 +0300

    module1-task3

---
+++ src/components/main/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main className="page__main page__main--index">
+      <h1 className="visually-hidden">Cities</h1>
+    </main>
+  );
+};
+
+export default Main;
